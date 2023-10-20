6ee37c02a43ddd719f0d6bc4d39bd6918df71f37
Alfie Hiscox <alfie.hiscox@gmail.com>
2023-10-20 17:15:44 +0100

    Adding object representation of integers and finishing repl

---
+++ evaluator/evaluator.go
@@ -0,0 +1,29 @@
+package evaluator
+
+import (
+	"github.com/alfiehiscox/monkey-go/ast"
+	"github.com/alfiehiscox/monkey-go/object"
+)
+
+func Eval(node ast.Node) object.Object {
+	switch node := node.(type) {
+	// Statements
+	case *ast.Program:
+		return evalStatements(node.Statements)
+	case *ast.ExpressionStatement:
+		return Eval(node.Expression)
+	// Expressions
+	case *ast.IntegerLiteral:
+		return &object.Integer{Value: node.Value}
+	}
+
+	return nil
+}
+
+func evalStatements(stmts []ast.Statement) object.Object {
+	var result object.Object
+	for _, stmt := range stmts {
+		result = Eval(stmt)
+	}
+	return result
+}
